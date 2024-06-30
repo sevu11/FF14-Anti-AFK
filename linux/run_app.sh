0c7e873bfb809099441ee19e534ef468fe51ca04
sevu11 <johan.sorman@icloud.com>
2024-06-30 13:29:03 +0200

    Update run_app.sh

--- linux/run_app.sh
+++ linux/run_app.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Create Virtual enviroment
+# Refer as venv
+python3 -m venv venv 
+
+# Activate venv 
 source venv/bin/activate
+
+# Upgrade pip module
+pip install --upgrade pip
+
+# Install required packages
 pip install -r requirements.txt
+
+# Run the application
 python3 main.py
+
+# Deactivate venv
 deactivate
