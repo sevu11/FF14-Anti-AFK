a3996d2dc27ceec5daa0182f504db8c4e1b6ac82
sevu11 <johan.sorman@icloud.com>
2024-06-30 13:25:37 +0200

    Update to run script and requirements

--- linux/run_app.sh
+++ linux/run_app.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# Create virtual environment
-python3 -m venv venv
-
-# Activate virtual environment
 source venv/bin/activate
-
-# Install required packages
 pip install -r requirements.txt
-
-# Run your Python script
 python3 main.py
-
-# Deactivate virtual environment (optional)
 deactivate
